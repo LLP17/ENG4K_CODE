87f38b7b34575278c0feb921557d47bce04675a1
Enzo Panem <epanem@my.yorku.ca>
2025-03-10 13:11:39 -0400

    Update motor

--- c-webserver/webserver/motor.ino
+++ c-webserver/webserver/motor.ino
@@ -1,22 +1,39 @@
 void controlMotor(const char* direction, int distance)
 {
-    if (strcmp(direction, "FORWARD") == 0)
+    if (strcmp(direction, "Top") == 0)
     {
         handleForward(distance);
     }
-    else if (strcmp(direction, "BACKWARD") == 0)
+    else if (strcmp(direction, "Bottom") == 0)
     {
         handleBackward(distance);
     }
-    else if (strcmp(direction, "LEFT") == 0)
+    else if (strcmp(direction, "Left") == 0)
     {
         handleLeft(distance);
     }
-    else if (strcmp(direction, "RIGHT") == 0)
+    else if (strcmp(direction, "Right") == 0)
     {
         handleRight(distance);
+    } 
+    else if (strcmp(direction, "RightTop") == 0) 
+    {
+        handleForwardRight(distance);
+    } 
+    else if (strcmp(direction, "TopLeft") == 0) 
+    {
+        handleForwardLeft(distance);
+
     }
-    else if (strcmp(direction, "STOP") == 0)
+    else if (strcmp(direction, "LeftBottom") == 0) 
+    {   
+        handleBackwardLeft(distance);
+    }
+    else if (strcmp(direction, "BottomRight") == 0) 
+    {
+        handleBackwardRight(distance);
+    }
+    else if (strcmp(direction, "STOP") == 0 || strcmp(direction, "Center") == 0)
     {
         handleStop(distance);
     }
@@ -30,6 +47,14 @@ void handleForward(int distance)
 {
     Serial.printf("[ACTIVE] DRIVE Direction: FORWARD, Distance: %d\n", distance);
 
+}
+
+void handleForwardRight(int distance) {
+    Serial.printf("[ACTIVE] DRIVE Direction: FORWARD-RIGHT, Distance: %d\n", distance);
+}
+
+void handleForwardLeft(int distance) {
+    Serial.printf("[ACTIVE] DRIVE Direction: FORWARD-LEFT, Distance: %d\n", distance);
 }
 
 void handleLeft(int distance)
@@ -53,5 +78,17 @@ void handleRight(int distance)
 void handleBackward(int distance)
 {
     Serial.printf("[ACTIVE] DRIVE Direction: BACKWARD, Distance: %d\n", distance);
+    
+}
+
+void handleBackwardLeft(int distance)
+{
+    Serial.printf("[ACTIVE] DRIVE Direction: BACKWARD-LEFT, Distance: %d\n", distance);
+    
+}
+
+void handleBackwardRight(int distance)
+{
+    Serial.printf("[ACTIVE] DRIVE Direction: BACKWARD-RIGHT, Distance: %d\n", distance);
     
 }
