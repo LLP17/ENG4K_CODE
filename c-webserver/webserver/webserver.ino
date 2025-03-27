cbd6d575ec79f509b34070eb92f7e72300b944ce
Enzo Panem <epanem@my.yorku.ca>
2025-03-26 21:38:49 -0400

    Video works

--- c-webserver/webserver/webserver.ino
+++ c-webserver/webserver/webserver.ino
@@ -3,7 +3,13 @@
 #include <ESPAsyncWebServer.h>
 #include <SPIFFS.h>
 #include <ArduinoJson.h>
+#include "esp_camera.h"
 
+#define CAMERA_MODEL_ESP32S3_EYE
+#include "camera_pins.h"
+#include "app_httpd.h"  // <- Import camera streaming module
+
+/* WiFi Access Point Configuration */
 const char *ssid_AP = "ECHO-AP";
 const char *password_AP = "ourbread";
 IPAddress local_IP(192, 168, 1, 100);
@@ -14,129 +20,152 @@ AsyncWebServer server(80);
 AsyncWebSocket ws("/ws");
 
 /* Motor Configurations */
-
-// Motor Pins
-int motor1Pin1 = 21; 
-int motor1Pin2 = 22; 
+int motor1Pin1 = 21;
+int motor1Pin2 = 22;
 int enable1Pin = 23;
-int motor2Pin1 = 5; 
-int motor2Pin2 = 19; 
+int motor2Pin1 = 5;
+int motor2Pin2 = 19;
 int enable2Pin = 18;
 
-const int freq = 30000;    // PWM frequency
-const int resolution = 8;  // PWM resolution (8-bit)
-const int channel1 = 0;    // PWM channel for motor 1
-const int channel2 = 1;    // PWM channel for motor 2
-int dutyCycle = 0;
+const int freq = 30000;
+const int pwm_resolution = 8;
+const int channel1 = 0;
+const int channel2 = 1;
 
-void handleWebSocketMessage(void *arg, uint8_t *data, size_t len)
-{
-    AwsFrameInfo *info = (AwsFrameInfo *)arg;
-    if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
-        DynamicJsonDocument doc(256);
-        DeserializationError error = deserializeJson(doc, data, len);
-        if (error) {
-            Serial.print(F("deserializeJson() failed with code "));
-            Serial.println(error.c_str());
-            return;
-        }
-        const char *direction = doc["direction"].as<const char*>();
-        double x = doc["x"].as<double>();
-        double y = doc["y"].as<double>();
-
-        // In "motor.ino"
-        controlMotor(direction, x, y);
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
+  AwsFrameInfo *info = (AwsFrameInfo *)arg;
+  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+    DynamicJsonDocument doc(256);
+    DeserializationError error = deserializeJson(doc, data, len);
+    if (error) {
+      Serial.print(F("deserializeJson() failed with code "));
+      Serial.println(error.c_str());
+      return;
     }
 
+    const char *direction = doc["direction"];
+    double x = doc["x"];
+    double y = doc["y"];
+    controlMotor(direction, x, y);
+  }
 }
 
-void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,
-             void *arg, uint8_t *data, size_t len)
-{
-    switch (type)
-    {
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client,
+             AwsEventType type, void *arg, uint8_t *data, size_t len) {
+  switch (type) {
     case WS_EVT_CONNECT:
-        Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
-        break;
+      Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+      break;
     case WS_EVT_DISCONNECT:
-        Serial.printf("WebSocket client #%u disconnected\n", client->id());
-        break;
+      Serial.printf("WebSocket client #%u disconnected\n", client->id());
+      break;
     case WS_EVT_DATA:
-        handleWebSocketMessage(arg, data, len);
-        break;
-    case WS_EVT_PONG:
-    case WS_EVT_ERROR:
-        break;
-    }
+      handleWebSocketMessage(arg, data, len);
+      break;
+    default:
+      break;
+  }
 }
 
-void initWebSocket()
-{
-    ws.onEvent(onEvent);
-    server.addHandler(&ws);
+void initWebSocket() {
+  ws.onEvent(onEvent);
+  server.addHandler(&ws);
 }
 
-void initWifi()
-{
-    WiFi.disconnect();
-    WiFi.mode(WIFI_AP);
-    if (WiFi.softAPConfig(local_IP, gateway, subnet))
-    {
-        Serial.println("AP Config Ready");
-    }
-    else
-    {
-        Serial.println("AP Config Failed!");
-    }
+void initWifi() {
+  WiFi.disconnect();
+  WiFi.mode(WIFI_AP);
+  if (WiFi.softAPConfig(local_IP, gateway, subnet)) {
+    Serial.println("AP Config Ready");
+  } else {
+    Serial.println("AP Config Failed!");
+  }
 
-    if (WiFi.softAP(ssid_AP, password_AP))
-    {
-        Serial.println("AP Started");
-        Serial.println("IP Address: " + WiFi.softAPIP().toString());
-    }
-    else
-    {
-        Serial.println("AP Failed to Start!");
-    }
+  if (WiFi.softAP(ssid_AP, password_AP)) {
+    Serial.println("AP Started");
+    Serial.println("IP Address: " + WiFi.softAPIP().toString());
+  } else {
+    Serial.println("AP Failed to Start!");
+  }
 }
 
-void setup()
-{
-    Serial.begin(115200);
-    if (!SPIFFS.begin(true))
-    {
-        Serial.println("Failed to mount SPIFFS");
-        return;
-    }
+void setup() {
+  Serial.begin(115200);
+  if (!SPIFFS.begin(true)) {
+    Serial.println("Failed to mount SPIFFS");
+    return;
+  }
 
-    /* Motor Setup */
+  /* Motor Setup */
+  pinMode(motor1Pin1, OUTPUT);
+  pinMode(motor1Pin2, OUTPUT);
+  pinMode(motor2Pin1, OUTPUT);
+  pinMode(motor2Pin2, OUTPUT);
+  ledcAttach(enable1Pin, freq, pwm_resolution);
+  ledcAttach(enable2Pin, freq, pwm_resolution);
+  ledcWrite(channel1, 0);
+  ledcWrite(channel2, 0);
 
-    // Motor pin setup
-    pinMode(motor1Pin1, OUTPUT);
-    pinMode(motor1Pin2, OUTPUT);
-    pinMode(motor2Pin1, OUTPUT);
-    pinMode(motor2Pin2, OUTPUT);
+  /* Camera Setup */
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sccb_sda = SIOD_GPIO_NUM;
+  config.pin_sccb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.frame_size = FRAMESIZE_SVGA;
+  config.pixel_format = PIXFORMAT_JPEG;
+  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+  config.fb_location = CAMERA_FB_IN_PSRAM;
+  config.jpeg_quality = 12;
+  config.fb_count = 2;
 
-    // PWM setup
-    // Configure PWM Pins
-    ledcAttach(enable1Pin, freq, resolution);
-    ledcAttach(enable2Pin, freq, resolution);
+  if (psramFound()) {
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+    config.grab_mode = CAMERA_GRAB_LATEST;
+  } else {
+    config.fb_count = 1;
+    config.fb_location = CAMERA_FB_IN_DRAM;
+  }
 
-    // Initialize PWM
-    ledcWrite(channel1, 0);
-    ledcWrite(channel2, 0);
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
 
-    /* Motor Setup END */
+  sensor_t *s = esp_camera_sensor_get();
+  s->set_vflip(s, 1);
+  s->set_brightness(s, 1);
+  s->set_saturation(s, 0);
 
-    initWifi();
+  /* Start Services */
+  initWifi();
+  initWebSocket();
+  server.serveStatic("/", SPIFFS, "/").setDefaultFile("index.html");
+  server.begin();
+  startCameraServer();
 
-    server.serveStatic("/", SPIFFS, "/").setDefaultFile("index.html");
-    server.begin();
-
-    initWebSocket();
+  Serial.print("Camera Ready! Use 'http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("' to connect");
 }
 
-void loop()
-{
-    ws.cleanupClients();
+void loop() {
+  ws.cleanupClients();
 }
